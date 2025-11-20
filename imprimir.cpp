88794ad311463a5c3fbe8f931f6bb05fb5157529
itzmefabry <rasf080924hgtnrba0@gmail.com>
2025-11-19 18:37:50 -0600

    Primera version de hola mundo

---
+++ imprimir.cpp
@@ -0,0 +1,10 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+// bloque int main - bloque principal
+int main()
+{
+    cout << "Hola mundo";
+    return 0;
+}
